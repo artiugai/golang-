b7a6bfa4a991a4818c948fb8fb132cf10160608f
Artyom Ugai <ugaygg@gmail.com>
2023-10-07 20:52:44 +0600

    added error messages

--- greenlight/internal/data/luxarywatches.go
+++ greenlight/internal/data/luxarywatches.go
@@ -1,16 +1,16 @@
 package data
 
-import (
-	"time"
-)
+import "time"
 
-// Watches represents information about a watches.
 type Watches struct {
-	ID          int64     // Unique integer ID for the watches
-	CreatedAt   time.Time // Timestamp for when the watches is added to our database
-	Title       string    // Watches title
-	Year        int32     // Watches release year
-	Runtime     int32     //  --
-	WatchesType []string  // Type of watches
-	Version     int32     // The version number starts at 1 and will be incremented each time the movie information is updated
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	// Use the Runtime type instead of int32.
+	// Note: The omitempty directive will still work on this: if the Runtime field has the
+	// underlying value 0, then it will be considered empty and omitted.
+	Runtime     Runtime  `json:"runtime,omitempty"`
+	WatchesType []string `json:"WatchesType,omitempty"`
+	Version     int32    `json:"version"`
 }
